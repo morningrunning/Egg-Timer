7f3dbf1b3c18d0eb5a0c77f07ae1d7648405711e
Ronald Morgan <rwmorgan01@yahoo.com>
2016-10-07 21:13:22 -0400

    Egg Timer

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer = Timer()
+    
+    var time = 210
 
+
+    func decreaseTimer(){
+        
+        if time > 0 {
+        time -= 1
+        timerLabel.text = String(time)
+        }else{
+        timer.invalidate()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playTimer(_ sender: AnyObject) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
 
+    @IBAction func pauseTimer(_ sender: AnyObject) {
+        timer.invalidate()
+    }
+  
+    @IBAction func minusTen(_ sender: AnyObject) {
+        if time > 10{
+        time -= 10
+        timerLabel.text = String(time)
+        }
+    }
+    
+    @IBAction func plusTen(_ sender: AnyObject) {
+        time += 10
+    }
+    
+    @IBAction func reset(_ sender: AnyObject) {
+        time = 210
+        timerLabel.text = String(time)
+    }
 }
 
